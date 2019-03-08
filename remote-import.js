d394158e1b16875f0b629a9629fa20ce00e7f348
Ben Parsons <ben@bpulse.co.uk>
2019-03-08 18:29:28 +0000

    set current directory

--- remote-import.js
+++ remote-import.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+process.chdir(__dirname);
 const { exec } = require('child_process');const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./visible.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
